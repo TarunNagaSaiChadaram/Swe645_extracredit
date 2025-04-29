a1ec6e904d3a87b32b407f419dbe50cdf2289ed3
TarunNagaSaiChadaram <162387068+TarunNagaSaiChadaram@users.noreply.github.com>
2025-04-29 13:59:18 -0400

    Add files via upload

---
+++ stusurvey-pythonflask/app.py
@@ -0,0 +1,90 @@
+from flask import Flask, request, jsonify
+from database import db
+from models import StudentSurvey
+from config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    # Routes
+    @app.route('/surveys', methods=['GET'])
+    def get_surveys():
+        surveys = StudentSurvey.query.all()
+        return jsonify([s.to_dict() for s in surveys])
+
+    @app.route('/surveys/<int:survey_id>', methods=['GET'])
+    def get_survey(survey_id):
+        survey = StudentSurvey.query.get_or_404(survey_id)
+        return jsonify(survey.to_dict())
+
+    @app.route('/surveys', methods=['POST'])
+    def create_survey():
+        data = request.get_json()
+
+        try:
+            new_survey = StudentSurvey(
+                first_name=data['first_name'],
+                last_name=data['last_name'],
+                street_address=data['street_address'],
+                city=data['city'],
+                state=data['state'],
+                zip_code=data['zip_code'],
+                phone=data['phone'],
+                email=data['email'],
+                campus_likes=data.get('campus_likes'),
+                campus_interests=data.get('campus_interests'),
+                recommend=data['recommend']
+            )
+            db.session.add(new_survey)
+            db.session.commit()
+            return jsonify(new_survey.to_dict()), 201
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({"error": str(e)}), 400
+
+    @app.route('/surveys/<int:survey_id>', methods=['PUT'])
+    def update_survey(survey_id):
+        survey = StudentSurvey.query.get_or_404(survey_id)
+        data = request.get_json()
+
+        try:
+            survey.first_name = data['first_name']
+            survey.last_name = data['last_name']
+            survey.street_address = data['street_address']
+            survey.city = data['city']
+            survey.state = data['state']
+            survey.zip_code = data['zip_code']
+            survey.phone = data['phone']
+            survey.email = data['email']
+            survey.campus_likes = data.get('campus_likes')
+            survey.campus_interests = data.get('campus_interests')
+            survey.recommend = data['recommend']
+
+            db.session.commit()
+            return jsonify(survey.to_dict())
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({"error": str(e)}), 400
+
+    @app.route('/surveys/<int:survey_id>', methods=['DELETE'])
+    def delete_survey(survey_id):
+        survey = StudentSurvey.query.get_or_404(survey_id)
+        try:
+            db.session.delete(survey)
+            db.session.commit()
+            return jsonify({"message": "Survey deleted successfully."})
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({"error": str(e)}), 400
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host='0.0.0.0', port=5001)
---
+++ stusurvey-pythonflask/config.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://admin:database645swe@database-645.cylxhmwgst1w.us-east-1.rds.amazonaws.com/stusurvec"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
