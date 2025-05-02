dcb0a27b8c9575ba6cf181dd6d9b600a81755ca3
TarunNagaSaiChadaram <162387068+TarunNagaSaiChadaram@users.noreply.github.com>
2025-05-01 20:16:42 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+# - Samanvitha Matta (G01252738)
+# - Akshaya Reddy Dundigalla (G01482843)
+# - Tarun Naga Sai Chadaram (G01445928)
+# Flask application configuration class to connect to Amazon RDS MySQL database
+
 class Config:
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://admin:database645swe@database-645.cylxhmwgst1w.us-east-1.rds.amazonaws.com/stusurvec"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
