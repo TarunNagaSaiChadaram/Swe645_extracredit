ce0d03fe7c9b1a65f58c391ed9e566027f911f93
TarunNagaSaiChadaram <162387068+TarunNagaSaiChadaram@users.noreply.github.com>
2025-05-01 20:17:59 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,3 +1,8 @@
+# - Samanvitha Matta (G01252738)
+# - Akshaya Reddy Dundigalla (G01482843)
+# - Tarun Naga Sai Chadaram (G01445928)
+# Sets up the SQLAlchemy database object for the Flask application.
+# To manage ORM activities, this will be imported into the main application.
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
